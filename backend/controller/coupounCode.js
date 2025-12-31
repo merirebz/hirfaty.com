f2e52a1354d5ab064b8c60edbcfd6418ca6c4869
Mery <149592766+merirebz@users.noreply.github.com>
2025-12-31 19:25:18 +0000

    Update coupounCode.js

--- backend/controller/coupounCode.js
+++ backend/controller/coupounCode.js
@@ -6,7 +6,6 @@ const { isSeller } = require("../middleware/auth");
 const CoupounCode = require("../model/coupounCode");
 const router = express.Router();
 
-// create coupoun code
 router.post(
   "/create-coupon-code",
   isSeller,
@@ -32,7 +31,7 @@ router.post(
   })
 );
 
-// get all coupons of a shop
+
 router.get(
   "/get-coupon/:id",
   isSeller,
@@ -70,7 +69,7 @@ router.delete(
   })
 );
 
-// get coupon code value by its name
+
 router.get(
   "/get-coupon-value/:name",
   catchAsyncErrors(async (req, res, next) => {
